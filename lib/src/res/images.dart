7d8313550f250bc6ab949097eb79b9264e69573d
Yczar <babalolagbogo@gmail.com>
2022-10-05 19:43:16 +0100

    created files to store assets directories

---
+++ lib/src/res/images.dart
@@ -0,0 +1,6 @@
+const String _imgDir = 'assets/images';
+const String _onboardingImgDir = '$_imgDir/onboarding';
+
+/// [OnboardingScreen] images
+const String kImgOnboardingFeatured =
+    '$_onboardingImgDir/onboarding_featured.png';
---
+++ lib/src/res/svgs.dart
@@ -0,0 +1,15 @@
+const String _svgDir = 'assets/svgs';
+const String _mainSvgDir = '$_svgDir/main';
+const String _homeSvgDir = '$_svgDir/home';
+
+/// [Main]
+const String kMainHomeSvg = '$_mainSvgDir/home.svg';
+const String kMainPaperSvg = '$_mainSvgDir/paper.svg';
+const String kMainProfileSvg = '$_mainSvgDir/profile.svg';
+const String kMainDiscountSvg = '$_mainSvgDir/discount.svg';
+const String kMainBackArrowSvg = '$_mainSvgDir/back_arrow.svg';
+const String kMainMoreSvg = '$_mainSvgDir/more.svg';
+
+/// [Home]
+const String kHomeFilterSvg = '$_homeSvgDir/filter.svg';
+const String kHomeNotificationSvg = '$_homeSvgDir/notification.svg';
