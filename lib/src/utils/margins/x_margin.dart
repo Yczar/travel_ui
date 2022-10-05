b4ce8752582de2d34a587272fea79c790d4c1bcd
Yczar <babalolagbogo@gmail.com>
2022-10-05 19:42:21 +0100

    created: the XMargin and YMargin files that serves as a margin for the x and y axis of a widget respectively

---
+++ lib/src/utils/margins/x_margin.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class XMargin extends StatelessWidget {
+  const XMargin(this.value, {Key? key}) : super(key: key);
+  final double? value;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: value,
+    );
+  }
+}
