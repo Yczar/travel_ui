dfb9ae67fa070b3a9baeab813a1b96451d61b3ff
Yczar <babalolagbogo@gmail.com>
2022-10-05 19:42:31 +0100

    created: a colors.dart file

---
+++ lib/src/values/colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const Color kPrimaryColor = Color(0xFF1D3FFF);
+const Color kSecondaryColor = Color(0xFFEFF4FF);
+const Color kGrayColor = Color(0xFFBFBFBF);
