6c3897afa28d55e3f07f71b1f8409253a0e9f15a
Yczar <babalolagbogo@gmail.com>
2022-10-05 19:02:37 +0100

    created: the onboardong model

---
+++ lib/app/models/onboarding.dart
@@ -0,0 +1,48 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
+import 'package:equatable/equatable.dart';
+
+class Onboarding extends Equatable {
+  final String title;
+  final String description;
+  const Onboarding({
+    required this.title,
+    required this.description,
+  });
+
+  Onboarding copyWith({
+    String? title,
+    String? description,
+  }) {
+    return Onboarding(
+      title: title ?? this.title,
+      description: description ?? this.description,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'title': title,
+      'description': description,
+    };
+  }
+
+  factory Onboarding.fromMap(Map<String, dynamic> map) {
+    return Onboarding(
+      title: map['title'] as String,
+      description: map['description'] as String,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Onboarding.fromJson(String source) =>
+      Onboarding.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  @override
+  bool get stringify => true;
+
+  @override
+  List<Object> get props => [title, description];
+}
